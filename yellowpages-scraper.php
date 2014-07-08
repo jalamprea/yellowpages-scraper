b04b5f127a458181ef5092275a143f810146f081
opz <opz@ubuntu.(none)>
2014-07-08 14:36:23 -0400

    updated scraper to work with DOM updates to yellowpages.com

--- yellowpages-scraper.php
+++ yellowpages-scraper.php
@@ -15,13 +15,13 @@ class YellowpagesScraper {
         $search = 'Financial Advisors';
         $url = 'http://www.yellowpages.com/financial-district-new-york-ny/financial-advisors';
          */
-        $name = 'yellowpages.com';
+        $name     = 'yellowpages.com';
         $location = 'New York, NY';
-        $search = 'Jewelry Stores';
-        $url = 'http://www.yellowpages.com/new-york-ny/jewelry-stores';
+        $search   = 'Jewelry Stores';
+        $url      = 'http://www.yellowpages.com/new-york-ny/jewelry-stores';
 
         $scanModules = array(
-            'Google Analytics' => 'googleAnalytics',
+            //'Google Analytics' => 'googleAnalytics',
             'Shopify' => 'shopify'
             //'Twitter Bootstrap' => 'bootstrap'
         );
@@ -50,10 +50,10 @@ class YellowpagesScraper {
         $html = str_get_html(getHTML($url, $data));
 
         $entries = array();
-        foreach($html->find('.result-container') as $listing) { 
-            $name = $listing->find('div.business-name-container, div.srp-business-name', 0);
-            $link = $listing->find('div.info-business-additional a.track-visit-website', 0);
-            $moreinfo = $listing->find('a.track-more-info', 0);
+        foreach($html->find('.result') as $listing) { 
+            $name = $listing->find('a.business-name span', 0);
+            $link = $listing->find('ul.links a.track-visit-website', 0);
+            $moreinfo = $listing->find('ul.links a.track-more-info', 0);
             $entries[] = array(
                 $name ? trim(htmlspecialchars_decode($name->plaintext)) : null,
                 $link ? $link->href : null,
